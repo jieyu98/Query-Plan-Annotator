5dea66f7b76919fc63f616f207c02a347ef590db
jieyu98 <33819433+jieyu98@users.noreply.github.com>
2022-11-05 02:24:22 +0800

    Update

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,16 @@
 from psycopg2 import connect
 
+DEFAULT_SEQ_PAGE_COST = 1.0
+DEFAULT_RAND_PAGE_COST = 5.0
+
+
 class Connection:
     def __init__(self):
-        print("Hello world")
+        print("Beginning connection")
         self.conn = self.begin_connection()
         self.cursor = self.conn.cursor()
 
     def begin_connection(self):
-        """
-            Begin connection with the Postgresql database
-            Returns:
-                connect: connection to database
-        """
         return connect(
             dbname="TPC-H",
             user="jy",
@@ -28,39 +27,59 @@ class Connection:
         self.cursor.execute("SET seq_page_cost TO " + str(seq_page))
         self.cursor.execute("SET random_page_cost TO " + str(rand_page))
 
-    def execute_query(self, query, seq_cost=1, rand_cost=1):
-        # seq_cost and rand_cost are set by default to 1 in the above params
-        # no need to set in get_qep
-        self.change_parameters(seq_cost, rand_cost)
-        self.cursor.execute(query)
-        query_plan = self.cursor.fetchall()
+    def execute_query(self, query, seq_cost, rand_cost):
+        self.change_parameters(seq_cost, rand_cost)  # Set parameters of the query
+        self.cursor.execute(query)  # Execute query
+        query_plan = self.cursor.fetchall()  # Fetch the query plan
         return query_plan
 
     def get_qep(self, query):
-        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query))
+        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
+                                        DEFAULT_SEQ_PAGE_COST, DEFAULT_RAND_PAGE_COST)
         query_plan = query_plan[0][0][0]['Plan']
         return query_plan
 
-    def get_aqp_1(self, query):
-        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query), 1, 3)
+    def get_aqp1(self, query):
+        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
+                                        DEFAULT_SEQ_PAGE_COST+3, DEFAULT_RAND_PAGE_COST+1)
         query_plan = query_plan[0][0][0]['Plan']
         return query_plan
 
-    def get_aqp_2(self, query):
-        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query), 1, 0)
+    def get_aqp2(self, query):
+        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
+                                        DEFAULT_SEQ_PAGE_COST+5, DEFAULT_RAND_PAGE_COST+2)
         query_plan = query_plan[0][0][0]['Plan']
         return query_plan
+
+    # Recursion method
+    def get_nodes(self, query_plan):
+        if 'Plans' in query_plan.keys():
+            for plan in query_plan["Plans"]:
+                print(plan['Node Type'])
+                self.get_nodes(plan)
+
+    # Iterative method
+    def get_nodes(self, query_plan):
+        level = 0
+
 
 def main():
-    # below query is only for testing
-    output = query_processor.get_qep(
-        "SELECT *" +
-        "FROM customer C, orders O " +
-        "WHERE " +
-        "C.c_custkey = O.o_custkey " +
-        "LIMIT 10"
-    )
-    print(output)
+    # test_query = "SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey LIMIT 10"
+    test_query = "SELECT l_orderkey, sum(l_extendedprice * (1 - l_discount)) as revenue, o_orderdate, o_shippriority " +\
+    "FROM customer, orders, lineitem " +\
+    "WHERE c_mktsegment = 'BUILDING' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate < date '1995-03-15' AND l_shipdate > date '1995-03-15' " +\
+    "GROUP BY l_orderkey, o_orderdate, o_shippriority " +\
+    "ORDER BY revenue desc, o_orderdate LIMIT 20"
+
+    qep = query_processor.get_qep(test_query)
+    query_processor.get_nodes(qep)
+    # aqp1 = query_processor.get_aqp1(test_query)
+    # aqp2 = query_processor.get_aqp2(test_query)
+
+    print(qep)
+    # print(aqp1)
+    # print(aqp2)
+
     return
 
 
