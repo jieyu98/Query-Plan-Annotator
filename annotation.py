3acd94019bdb628b73ffa1d5818688d3322c8fbd
jieyu98 <33819433+jieyu98@users.noreply.github.com>
2022-11-06 01:45:29 +0800

    Updates

---
+++ annotation.py
@@ -0,0 +1,126 @@
+from preprocessing import main
+
+class Annotate:
+    def get_annotation(self, node):
+        main_explaination = "NIL"
+        additional_info = ""
+
+        if node['Node Type'] == 'Limit':
+            main_explaination = "Limit is used because only a certain number of rows are required"
+
+        if node['Node Type'] == 'Hash Join':
+            main_explaination = "Hash join is used as there is a hash table created in one of the tables. "
+            additional_info = f"Hash Join is used on the condition {node['Hash Cond']}"
+
+        if node['Node Type'] == 'Seq Scan':
+            main_explaination = "Sequential scan is done because there is no index on the table data or when fetching a few rows from a large table"
+            additional_info = f"Sequential Scan is performed on the relation: {node['Relation Name']}"
+
+        if node['Node Type'] == 'Hash':
+            main_explaination = "Hash is used due to the table is the smaller one and thus minimal memory is required to store the hash table in memory, or for a future hash join"
+            additional_info = f"Hash used {node['Hash Buckets']} buckets"
+
+        if node['Node Type'] == 'Merge Join':
+            main_explaination = "Merge Join is used because the tables are sorted and uses minimal memory"
+            additional_info = f"Merge join is used on the condition {node['Condition']}"
+
+        if node['Node Type'] == 'Sort':
+            main_explaination = "Sort is done for a merge-join to carry out in the later stages"
+            additional_info = f"Sort is done on the key {node['Sort Key']}"
+
+        if node['Node Type'] == 'Index Scan':
+            main_explaination = "Index Scan is done as there is an index on the tables, thus index scan having lower cost as compared to Sequential Scan"
+            additional_info = f"Index Scan is done on the conditions {node['Index Cond']}" \
+                              f"Index Scan is done using filter {node['Filter']}" \
+                              f"There were {node['Rows Removed by Filter']} rows removed by the filter"
+
+        if node['Node Type'] == 'Bitmap Scan':
+            main_explaination = "Bitmap Scan is used as the middle ground between index scan and sequential scan, where the number of records chosen may be too much for index scan but too little for sequential scan"
+
+        if node['Node Type'] == 'Aggregate':
+            main_explaination = "the results from this query plan are aggregated, due to the group-by function"
+            additional_info = f"The aggregation is done on the keys {node['Group Key']}"    
+
+        if node['Node Type'] == 'Nested Loop':
+            main_explaination = "Nested loop is used when the records to be looked for is small and the joined columns of the inner row source are uniquely indexed."
+
+        if node['Node Type'] == 'Index Only Scan':
+            main_explaination = "Index scan will access data only through the index and not the table, reducing IO cost, this is used because there is no need to access table data, only index data, for the results"
+            additional_info = f"Index scan involved {node['Plan Rows']} rows"
+
+        if node['Node Type'] == 'Subquery Scan':
+            main_explaination = "Subquery Scan will scan through the results from a child query"
+
+        return main_explaination, additional_info
+
+    def print_plan_plain(self, node_dict):
+        for i in node_dict:
+            if i == 0:
+                node = node_dict[i][0]
+                print(f"{node['Node Type']}")
+            else:
+                if len(node_dict[i]) == 1:
+                    node = node_dict[i][0]
+                    for j in range(0, i):
+                        print("\t", end='')
+                    print("└── ", f"{node['Node Type']}")
+                else:
+                    # More than 1 plans
+                    for j in range(0, i):
+                        print("\t", end='')
+                    node = node_dict[i][0]
+                    print("├── ", f"{node['Node Type']}")
+
+                    for j in range(0, i):
+                        print("\t", end='')
+                    node = node_dict[i][1]
+                    print("└── ", f"{node['Node Type']}")
+
+        print(f"Total Cost: {node_dict[i][0]['Total Cost']}")
+        print(f"Total Time: {node_dict[i][0]['Actual Total Time']}")
+
+    def print_plan_with_annotation(self, node_dict):
+        for i in node_dict:
+            if i == 0:
+                node = node_dict[i][0]
+                main_explaination, additional_info = self.get_annotation(node)
+                print(f"\033[1m{node['Node Type']}\033[0m ({main_explaination})")
+            else:
+                node = node_dict[i][0]
+                for j in range(0, i):
+                    print("\t", end='')
+                main_explaination, additional_info = self.get_annotation(node)
+                print("->  ", f"\033[1m{node['Node Type']}\033[0m ({main_explaination})")
+                for j in range(0, i):
+                    print("\t", end='')
+                print(f"     {additional_info}")
+
+                if(len(node_dict[i]) > 1):
+                    for j in range(0, i):
+                        print("\t", end='')
+                    node = node_dict[i][1]
+                    main_explaination, additional_info = self.get_annotation(node)
+                    print("->  ", f"\033[1m{node['Node Type']}\033[0m ({main_explaination})")
+                    for j in range(0, i):
+                        print("\t", end='')
+                    print(f"     {additional_info}")
+
+
+class main():
+    qep_node_dict, aqp1_node_dict, aqp2_node_dict = main()
+
+    annotation = Annotate()
+    print("                                                            QUERY PLAN STRUCTURE                                                            ")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------")
+    annotation.print_plan_plain(qep_node_dict)
+    print("                                                                 QUERY PLAN                                                                 ")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------")
+    annotation.print_plan_with_annotation(qep_node_dict)
+    print("                                                              AQP 1 STRUCTURE                                                               ")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------")
+    annotation.print_plan_plain(aqp1_node_dict)
+    print("                                                              AQP 2 STRUCTURE                                                               ")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------")
+    annotation.print_plan_plain(aqp2_node_dict)
+
+    
--- preprocessing.py
+++ preprocessing.py
@@ -26,6 +26,7 @@ class Connection:
     def change_parameters(self, seq_page, rand_page):
         self.cursor.execute("SET seq_page_cost TO " + str(seq_page))
         self.cursor.execute("SET random_page_cost TO " + str(rand_page))
+        # self.cursor.execute("SET enable_seqscan TO " + str("False"))
 
     def execute_query(self, query, seq_cost, rand_cost):
         self.change_parameters(seq_cost, rand_cost)  # Set parameters of the query
@@ -41,47 +42,63 @@ class Connection:
 
     def get_aqp1(self, query):
         query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
-                                        DEFAULT_SEQ_PAGE_COST+3, DEFAULT_RAND_PAGE_COST+1)
+                                        DEFAULT_SEQ_PAGE_COST + 5, DEFAULT_RAND_PAGE_COST + 5)
         query_plan = query_plan[0][0][0]['Plan']
         return query_plan
 
     def get_aqp2(self, query):
         query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
-                                        DEFAULT_SEQ_PAGE_COST+5, DEFAULT_RAND_PAGE_COST+2)
+                                        DEFAULT_SEQ_PAGE_COST + 10, DEFAULT_RAND_PAGE_COST + 10)
         query_plan = query_plan[0][0][0]['Plan']
         return query_plan
 
-    # Recursion method
-    def get_nodes(self, query_plan):
-        if 'Plans' in query_plan.keys():
-            for plan in query_plan["Plans"]:
-                print(plan['Node Type'])
-                self.get_nodes(plan)
+    def process_plan(self, plan):
+        temp = {}
 
-    # Iterative method
-    def get_nodes(self, query_plan):
-        level = 0
+        for i in plan:
+            if i != 'Plans':
+                temp[i] = plan[i]
+        return temp
+
+    # Recursion method
+    def get_nodes(self, query_plan, level, node_dict):
+        if level == 0:
+            node_dict[level] = [self.process_plan(query_plan)]
+
+        if 'Plans' in query_plan.keys():
+            level += 1
+            for plan in query_plan['Plans']:
+                # Check if level already exists
+                if level in node_dict:
+                    # Append to the existing array if level already exists
+                    node_dict[level].append(self.process_plan(plan))
+                else:
+                    node_dict[level] = [self.process_plan(plan)]
+
+                self.get_nodes(plan, level, node_dict)
 
 
 def main():
-    # test_query = "SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey LIMIT 10"
-    test_query = "SELECT l_orderkey, sum(l_extendedprice * (1 - l_discount)) as revenue, o_orderdate, o_shippriority " +\
-    "FROM customer, orders, lineitem " +\
-    "WHERE c_mktsegment = 'BUILDING' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate < date '1995-03-15' AND l_shipdate > date '1995-03-15' " +\
-    "GROUP BY l_orderkey, o_orderdate, o_shippriority " +\
-    "ORDER BY revenue desc, o_orderdate LIMIT 20"
+    query_processor = Connection()
+
+    test_query = "SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey"
+    # test_query = "SELECT l_orderkey, sum(l_extendedprice * (1 - l_discount)) as revenue, o_orderdate, o_shippriority " + \
+    #              "FROM customer, orders, lineitem " + \
+    #              "WHERE c_mktsegment = 'BUILDING' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND " \
+    #              "o_orderdate < date '1995-03-15' AND l_shipdate > date '1995-03-15' " + \
+    #              "GROUP BY l_orderkey, o_orderdate, o_shippriority " + \
+    #              "ORDER BY revenue desc, o_orderdate LIMIT 20"
+
+    qep_node_dict = {}
+    aqp1_node_dict = {}
+    aqp2_node_dict = {}
 
     qep = query_processor.get_qep(test_query)
-    query_processor.get_nodes(qep)
-    # aqp1 = query_processor.get_aqp1(test_query)
-    # aqp2 = query_processor.get_aqp2(test_query)
+    query_processor.get_nodes(qep, 0, qep_node_dict)
 
-    print(qep)
-    # print(aqp1)
-    # print(aqp2)
+    aqp1 = query_processor.get_aqp1(test_query)
+    query_processor.get_nodes(aqp1, 0, aqp1_node_dict)
 
-    return
-
-
-query_processor = Connection()
-main()
+    aqp2 = query_processor.get_aqp2(test_query)
+    query_processor.get_nodes(aqp2, 0, aqp2_node_dict)
+    return qep_node_dict, aqp1_node_dict, aqp2_node_dict
