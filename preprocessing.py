c8eb3505e1272d00f1ef1609e40bc3fe76e5cf4a
jieyu98 <33819433+jieyu98@users.noreply.github.com>
2022-11-11 16:36:32 +0800

    Clean up code

--- preprocessing.py
+++ preprocessing.py
@@ -4,12 +4,13 @@ DEFAULT_SEQ_PAGE_COST = 1.0
 DEFAULT_RAND_PAGE_COST = 5.0
 
 
-class Connection:
+class Preprocessor:
     def __init__(self):
         print("Beginning connection")
         self.conn = self.begin_connection()
         self.cursor = self.conn.cursor()
 
+    # To begin connection
     def begin_connection(self):
         return connect(
             dbname="TPC-H",
@@ -19,28 +20,32 @@ class Connection:
             host="localhost",
         )
 
+    # To stop connection
     def kill_connection(self):
         self.conn.close()
         self.cursor.close()
 
+    # To change sequential and random scan costs of a query
     def change_parameters(self, seq_page, rand_page):
         self.cursor.execute("SET seq_page_cost TO " + str(seq_page))
         self.cursor.execute("SET random_page_cost TO " + str(rand_page))
-        # self.cursor.execute("SET enable_seqscan TO " + str("False"))
 
-    def execute_query(self, query, seq_cost, rand_cost):
+    # Executes a query to retrieve the query plan
+    def execute_query(self, sql_statement, seq_cost, rand_cost):
         self.change_parameters(seq_cost, rand_cost)  # Set parameters of the query
-        self.cursor.execute(query)  # Execute query
+        self.cursor.execute(sql_statement)  # Execute query
         query_plan = self.cursor.fetchall()  # Fetch the query plan
         return query_plan
 
-    def get_qep(self, query):
-        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
+    # Calls execute_query function to get qep
+    def get_qep(self, sql_statement):
+        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + sql_statement),
                                         DEFAULT_SEQ_PAGE_COST, DEFAULT_RAND_PAGE_COST)
         query_plan = query_plan[0][0][0]['Plan']
         return query_plan
 
-    def get_aqp(self, query, disable_list):
+    # Function to call execute_query to get an aqp by disabling joins in disable_list
+    def get_aqp(self, sql_statement, disable_list):
         if 'Hash Join' in disable_list:
             self.cursor.execute('SET enable_hashjoin = off;')
 
@@ -50,11 +55,11 @@ class Connection:
         if 'Nested Loop' in disable_list:
             self.cursor.execute('SET enable_nestloop = off;')
 
-        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + query),
+        query_plan = self.execute_query(self.cursor.mogrify("EXPLAIN (ANALYZE, FORMAT JSON) " + sql_statement),
                                         DEFAULT_SEQ_PAGE_COST, DEFAULT_RAND_PAGE_COST)
         query_plan = query_plan[0][0][0]['Plan']
 
-        # Enable them back
+        # Enable everything back
         self.cursor.execute('SET enable_nestloop = on;')
         self.cursor.execute('SET enable_mergejoin = on;')
         self.cursor.execute('SET enable_hashjoin = on;')
@@ -69,7 +74,7 @@ class Connection:
                 temp[i] = plan[i]
         return temp
 
-    # Recursion method
+    # Get all the nodes in the query plan - Recursion method
     def get_nodes(self, query_plan, level, node_dict):
         if level == 0:
             node_dict[level] = [self.process_plan(query_plan)]
@@ -86,61 +91,63 @@ class Connection:
 
                 self.get_nodes(plan, level, node_dict)
 
+    def get_disable_list(self, disable_list, node_dict):
+        found = 0
+        for level in node_dict:
+            for plan in node_dict[level]:
+                # Check the node type if it is a join
+                if found == 0:
+                    if plan['Node Type'] == 'Hash Join':
+                        if len(disable_list) > 0:
+                            disable_list.append('Hash Join')
+                        else:
+                            disable_list = ['Hash Join']
+                        found = 1
+                    elif plan['Node Type'] == 'Merge Join':
+                        if len(disable_list) > 0:
+                            disable_list.append('Merge Join')
+                        else:
+                            disable_list = ['Merge Join']
+                        found = 1
+                    elif plan['Node Type'] == 'Nested Loop':
+                        if len(disable_list) > 0:
+                            disable_list.append('Nested Loop')
+                        else:
+                            disable_list = ['Nested Loop']
+                        found = 1
+
+        return disable_list
+
 
 def main():
-    query_processor = Connection()
+    query_processor = Preprocessor()
 
-    test_query = "SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey"
-    # test_query = "SELECT n_name,sum(l_extendedprice * (1 - l_discount)) as revenue FROM customer,orders,lineitem,supplier,nation,region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'ASIA' AND o_orderdate >= date '1994-01-01' AND o_orderdate < date '1994-01-01' + interval '1' year GROUP BY n_name ORDER BY revenue desc;"
-    # test_query = "SELECT l_orderkey, sum(l_extendedprice * (1 - l_discount)) as revenue, o_orderdate, o_shippriority " + \
-    #              "FROM customer, orders, lineitem " + \
-    #              "WHERE c_mktsegment = 'BUILDING' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND " \
-    #              "o_orderdate < date '1995-03-15' AND l_shipdate > date '1995-03-15' " + \
-    #              "GROUP BY l_orderkey, o_orderdate, o_shippriority " + \
-    #              "ORDER BY revenue desc, o_orderdate LIMIT 20"
+    sql_statement = "SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey"
 
     qep_node_dict = {}
     aqp1_node_dict = {}
     aqp2_node_dict = {}
 
-    qep = query_processor.get_qep(test_query)
+    # Get qep and populate qep_node_dict
+    qep = query_processor.get_qep(sql_statement)
     query_processor.get_nodes(qep, 0, qep_node_dict)
 
-    found = 0
-    for level in qep_node_dict:
-        for plan in qep_node_dict[level]:
-            # Check the node type if it is a join
-            if found == 0:
-                if plan['Node Type'] == 'Hash Join':
-                    disable_list = ['Hash Join']
-                    found = 1
-                elif plan['Node Type'] == 'Merge Join':
-                    disable_list = ['Merge Join']
-                    found = 1
-                elif plan['Node Type'] == 'Nested Loop':
-                    disable_list = ['Nested Loop']
-                    found = 1
+    disable_list = []
 
+    # Get disable list for aqp1
+    disable_list = query_processor.get_disable_list(disable_list, qep_node_dict)
     print(f"AQP1 Disabled: {disable_list}")
-    aqp1 = query_processor.get_aqp(test_query, disable_list)
+
+    # Get aqp1 and populate aqp1_node_dict
+    aqp1 = query_processor.get_aqp(sql_statement, disable_list)
     query_processor.get_nodes(aqp1, 0, aqp1_node_dict)
 
-    found = 0
-    for level1 in aqp1_node_dict:
-        for plan1 in aqp1_node_dict[level1]:
-            if found == 0:
-                if plan1['Node Type'] == 'Hash Join':
-                    disable_list.append('Hash Join')
-                    found = 1
-                elif plan1['Node Type'] == 'Merge Join':
-                    disable_list.append('Merge Join')
-                    found = 1
-                elif plan1['Node Type'] == 'Nested Loop':
-                    disable_list.append('Nested Loop')
-                    found = 1
-
+    # Get disable list for aqp2
+    disable_list = query_processor.get_disable_list(disable_list, aqp1_node_dict)
     print(f"AQP2 Disabled: {disable_list}")
-    aqp2 = query_processor.get_aqp(test_query, disable_list)
+
+    # Get aqp2 and populate aqp2_node_dict
+    aqp2 = query_processor.get_aqp(sql_statement, disable_list)
     query_processor.get_nodes(aqp2, 0, aqp2_node_dict)
 
     return qep_node_dict, aqp1_node_dict, aqp2_node_dict
